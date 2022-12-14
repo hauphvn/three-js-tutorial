8fe5f7aa735c4426e174c2d4df92343f8b159de3
hauphvn <pvhau@cmcglobal.vn>
2022-12-14 15:16:28 +0700

    add dist folder

---
+++ dist/server/server.js
@@ -0,0 +1,48 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const http = __importStar(require("http"));
+const path = __importStar(require("path"));
+const port = 3000;
+class App {
+    constructor(port) {
+        this.port = port;
+        const app = express();
+        app.use(express.static(path.join(__dirname, '../client')));
+        // This server.ts is only useful if you are running this on a production server or you
+        // want to see how the production version of bundle.js works
+        //
+        // to use this server.ts
+        // # npm run build        (this creates the production version of bundle.js and places it in ./dist/client/)
+        // # tsc -p ./src/server  (this compiles ./src/server/server.ts into ./dist/server/server.js)
+        // # npm start            (this starts node.js with express and serves the ./dist/client folder)
+        //
+        // visit http://127.0.0.1:3000
+        this.server = new http.Server(app);
+    }
+    Start() {
+        this.server.listen(this.port, () => {
+            console.log(`Server listening on port ${this.port}`);
+        });
+    }
+}
+new App(port).Start();
